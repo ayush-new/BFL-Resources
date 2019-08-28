5507c59e07e657084e1eb47e6b43b847e6ba9930
ayush-new <47331027+ayush-new@users.noreply.github.com>
2019-08-28 12:42:57 +0530

    Add files via upload

---
+++ xyz/mobileAppReference.js
@@ -0,0 +1,14 @@
+export const mobileAppLang = {
+mainHead:"SMILE MORE!",
+mainSubheading:"All new redesigned App",
+mainDescription:"Improved search, chat support and more new features for easy shopping",
+qrCodeDescription:"Get a link on your mobile to download the Brands For Less app!",
+buttonText:"Send SMS",
+
+a:"Keep Everything at Your Fingertips!",
+b:"",
+c:"",
+d:"",
+e:"",
+
+}
